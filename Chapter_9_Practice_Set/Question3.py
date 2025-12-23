6f35c947d0436b0276aecf76c706cbeac83586c8
deez-mama <bishowjeetb@gmail.com>
2025-12-23 16:26:03 +0545

    Saving Multiplcation tables done

--- Chapter_9_Practice_Set/Question3.py
+++ Chapter_9_Practice_Set/Question3.py
@@ -1,2 +1,9 @@
 # Write a program to generate multiplication tables from 2 to 20 and write it to the different files. Place these files in a folder for a 13 – year old.
 
+for i in range (2,21):
+    with open (f"Multiplication table {i}","w")as f:
+        for n in range (1,11):
+            product=n*i
+            f.write(str(product))
+            f.write(" ")
+
---
+++ Chapter_9_Practice_Set/Question4.py
@@ -0,0 +1,3 @@
+# A file contains a word “Donkey” multiple times. You need to write a program which replace this word with ##### by updating the same file.
+
+ 
